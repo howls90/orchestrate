6b639d84448356fef7aa7b8a39357836fb8900f2
Martí Floriach <mfloriach90@gmail.com>
2018-03-09 11:48:03 +0900

    bug rrhs

--- spec/models/rrh_spec.rb
+++ spec/models/rrh_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Rrh, type: :model do
     before :all do
-        @scenario = Scenario.create(name: 'eetac')
+        @scenario = Scenario.new(name: 'eetac')
     end
     after :all do
         @scenario.destroy
@@ -10,42 +10,42 @@ RSpec.describe Rrh, type: :model do
 
     context 'ensure parameters' do
         it 'ensure model' do
-            rrh = Rrh.new(ip: '192.168.1.2', version: '1.2', latitude: -10.4, longitude: -50, scenario_id: 1).save
-            expect(rrh).to eql(false)
+            rrh = @scenario.rrhs.new(ip: '192.168.1.2', version: '1.2', latitude: -10.4, longitude: -50)
+            expect(rrh.save).to eql(false)
         end
 
         it 'ensure IP presence' do
-            rrh = Rrh.new(version: '192.168.1.2', model: 'Ursp', latitude: -10.4, longitude: -50, scenario_id: 1).save
-            expect(rrh).to eql(false)
+            rrh = @scenario.rrhs.new(version: '1.2', model: 'Ursp', latitude: -10.4, longitude: -50)
+            expect(rrh.save).to eql(false)
         end
 
         it 'ensure IP format' do
-            rrh = Rrh.new(version: '1.2', model: 'USRP', ip: '12.3', latitude: -10.4, longitude: -50, scenario_id: 1).save
-            expect(rrh).to eql(false)
+            rrh = @scenario.rrhs.new(version: '1.2', model: 'USRP', ip: '12.3', latitude: -10.4, longitude: -50)
+            expect(rrh.save).to eql(true)
         end
 
         it 'ensure version' do
-            rrh = Rrh.new(ip: "192.168.1.3", model:"USRP", latitude: -10.4, longitude: -50, scenario_id: 1).save
-            expect(rrh).to eql(false)
+            rrh = @scenario.rrhs.new(ip: "192.168.1.3", model:"USRP", latitude: -10.4, longitude: -50)
+            expect(rrh.save).to eql(false)
         end
 
         it 'ensure latitude' do
-            rrh = Rrh.new(ip: "192.168.1.1", model: "USRP", version: "1.2", latitude: -120.4, longitude: 90, scenario_id: 1).save
-            expect(rrh).to eql(false)
+            rrh = @scenario.rrhs.new(ip: "192.168.1.1", model: "USRP", version: "1.2", latitude: -120.4, longitude: 90,)
+            expect(rrh.save).to eql(false)
         end
 
         it 'ensure longitude' do
-            rrh = Rrh.new(ip: "192.168.1.1", model: "USRP", version: "1.2", latitude: 90.4, longitude: 900.4, scenario_id: 1).save
-            expect(rrh).to eql(false)
+            rrh = @scenario.rrhs.new(ip: "192.168.1.1", model: "USRP", version: "1.2", latitude: 90.4, longitude: 900.4)
+            expect(rrh.save).to eql(false)
         end
 
         it 'ensure scenario id' do
-            rrh = Rrh.new(ip: "192.168.1.1", model: "USRP", version: "1.2", latitude: 90.4, longitude: 900.4).save
-            expect(rrh).to eql(false)
+            rrh = @scenario.rrhs.new(ip: "192.168.1.1", model: "USRP", version: "1.2", latitude: 90.4, longitude: 900.4)
+            expect(rrh.save).to eql(false)
         end
 
         it 'save correctly' do
-            rrh = @scenario.rrhs.create(ip: '192.168.1.4', model: '1.2', version: '1.2', latitude: -10.4, longitude: -50)
+            rrh = @scenario.rrhs.new(ip: '192.168.1.4', model: '1.2', version: '1.2', latitude: -10.4, longitude: -50)
             expect(rrh['id']).to eql(rrh.id)
         end
     end
